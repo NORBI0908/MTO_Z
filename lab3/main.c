007498b2b923ac22e7868e7ccd35de2df57aa8bf
Norbert Wilicki <nwilicki@tatuum.pl>
2022-10-23 14:54:28 +0200

    Adding #k

--- lab3/main.c
+++ lab3/main.c
@@ -7,20 +7,8 @@ int my_printf(char *format_string, char *param){
 	int a=0;
 	int i;
 	for(i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
-			char tekst[strlen(param)];
-			int j;
-			for(j=0;j<strlen(param);j++)
-			{
-				if(param[j]>=65&&param[j]<91) tekst[j] = tolower(param[j]);
-				else if(param[j]>=97&&param[j]<123) tekst[j] = toupper(param[j]);
-				else tekst[j] = param[j];
-			}
-			printf("%s",tekst);
-		}else if((format_string[i] == '#') && (format_string[i+1] == '.'))
+		if((format_string[i] == '#') && (format_string[i+1]>=40&&format_string[i+1]<58))
          {
-			 i++;
 			 i++;
 			 int k=0;
 			 while(format_string[i]>=40&&format_string[i]<58)
@@ -33,12 +21,13 @@ int my_printf(char *format_string, char *param){
 
 			 char newTekst[strlen(param)];
 			int m;
-			 for(m=0;m<a;m++)
+			 for(m=0;m<a&&m<strlen(param);m++)
 			 {
-				 newTekst[m] = param[m];
+				 if(param[m]>=65&&param[m]<91)  putchar(tolower(param[m]));
+				else if(param[m]>=97&&param[m]<123) putchar(toupper(param[m]));
+				else putchar(param[m]);
 			 }
-
-			 printf("%s",newTekst);
+			 
          }
          else
 			{
