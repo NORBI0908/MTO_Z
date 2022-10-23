65dd70a399983a1a42e0f6b9e16f4d540ab007ba
Norbert Wilicki <nwilicki@tatuum.pl>
2022-10-23 15:06:16 +0200

    Adding functionality from previous tasks

--- lab3/main.c
+++ lab3/main.c
@@ -7,7 +7,40 @@ int my_printf(char *format_string, char *param){
 	int a=0;
 	int i;
 	for(i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1]>=40&&format_string[i+1]<58))
+		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+			i++;
+			char tekst[strlen(param)];
+			int j;
+			for(j=0;j<strlen(param);j++)
+			{
+				if(param[j]>=65&&param[j]<91) tekst[j] = tolower(param[j]);
+				else if(param[j]>=97&&param[j]<123) tekst[j] = toupper(param[j]);
+				else tekst[j] = param[j];
+			}
+			printf("%s",tekst);
+		}else if((format_string[i] == '#') && (format_string[i+1] == '.'))
+         {
+			 i++;
+			 i++;
+			 int k=0;
+			 while(format_string[i]>=40&&format_string[i]<58)
+			 {
+				 a=a+((format_string[i]-'0')*pow(10,k));
+				 i++;
+				 k++;
+			 }
+			 
+
+			 char newTekst[strlen(param)];
+			int m;
+			 for(m=0;m<a;m++)
+			 {
+				 newTekst[m] = param[m];
+			 }
+
+			 printf("%s",newTekst);
+         }
+		 else if((format_string[i] == '#') && (format_string[i+1]>=40&&format_string[i+1]<58))
          {
 			 i++;
 			 int k=0;
