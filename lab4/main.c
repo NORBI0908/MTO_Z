59717db4e7126cf645470f80ec2692f513fce0b0
Norbert Wilicki <215916@edu.p.lodz.pl>
2022-11-06 15:17:53 +0100

    Fixing fgets

--- lab4/main.c
+++ lab4/main.c
@@ -3,7 +3,7 @@
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+		if((format_string[i] == '#') && (format_string[i+1] == 'g')){
 			i++;
 			printf("%s",param);
 		}else
@@ -15,8 +15,8 @@ int my_printf(char *format_string, char *param){
 
 int main(int argc, char *argv[]){
 	char buf[1024],buf2[1024];
-	while(gets(buf)){
-		gets(buf2);
+	while(fgets(buf,1024,stdin)){
+		fgets(buf2,1024,stdin);
 		my_printf(buf,buf2);
 	}
 	return 0;
